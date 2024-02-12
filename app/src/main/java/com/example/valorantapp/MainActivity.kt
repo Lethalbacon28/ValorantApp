4f324a482f083037fa64eef4d4d2fffdbbb0e1fd
LeafAsked <evanan0512@gmail.com>
2024-02-12 09:46:46 -0800

    Finished Binding in Main Activity

--- app/src/main/java/com/example/valorantapp/MainActivity.kt
+++ app/src/main/java/com/example/valorantapp/MainActivity.kt
@@ -2,14 +2,34 @@ package com.example.valorantapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.valorantapp.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    private lateinit var binding: ActivityMainBinding
+    companion object {
+        const val TAG = "MainActivity"
     }
 
-    private fun testing() {
-        print("meow")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val inputStream = resources.openRawResource(R.raw.earthquakes)
+        val jsonString = inputStream.bufferedReader().use {
+            it.readText()
+        }
+        Log.d(TAG, "OnCreate: jsonString $jsonString")
+        val gson = Gson()
+        val type = object: TypeToken<FeatureCollection>() {
+        }.type
+        earthquakes = gson.fromJson(jsonString, type)
+
+        Log.d(TAG,"LoadEarthquakes $earthquakes")
+
     }
 }
